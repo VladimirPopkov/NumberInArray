303341e52b2a77dfb767a6738f8db7757c7da1f7
Vladimir <vovanpopkov@mail.ru>
2017-08-21 21:33:50 +0300

    Новый проект

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,34 +5,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	package com.company;
-
-    import java.util.Scanner;
-
-        public class Main {
-
-            public static void main(String[] args) {
-                int number;
-                int a = 0;
-                int b = 0;
-                Scanner in = new Scanner(System.in);
-                System.out.print("Введите число: ");
-                number = in.nextInt();
-                b = number;
-                while (b != 0){
-                    b = b/10;
-                    a++;
-                }
-                int[] array = new int[a];
-                for (int i = 0; i < array.length; i++){
-                    array[array.length -1 - i] = number%10;
-                    number = number/10;
-                }
-                System.out.print("Преобразование в массив: ");
-                for (int i = 0; i < array.length; i++){
-                    System.out.print(array[i] + ", ");
-                }
-            }
+        int number;
+        int a = 0;
+        int b = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        number = in.nextInt();
+        b = number;
+        while (b != 0) {
+            b = b / 10;
+            a++;
+        }
+        int[] array = new int[a];
+        for (int i = array.length - 1; i >= 0; i--) {
+            array[i] = number % 10;
+            number = number / 10;
+        }
+        System.out.print("Преобразование в массив: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + ", ");
         }
     }
 }
+
